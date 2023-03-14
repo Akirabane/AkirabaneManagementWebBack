5a332ebe2eeacb0da05bd02f47b266055b602020
Akirabane <joshuabenon988@yahoo.fr>
2023-03-14 11:05:38 +0100

    Created Interface DAO players

---
+++ src/main/java/fr/akirabane/TestSpringFS/dao/IPlayerDao.java
@@ -0,0 +1,19 @@
+package fr.akirabane.TestSpringFS.dao;
+
+import fr.akirabane.TestSpringFS.entity.PlayerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface IPlayerDao extends JpaRepository<PlayerEntity, Integer> {
+    //find player by uuid
+    @Query("SELECT p FROM PlayerEntity p WHERE p.uuid_player = ?1")
+    Optional<PlayerEntity> findByUuid_player(String uuid_player);
+
+    //find player by pseudo
+    @Query("SELECT p FROM PlayerEntity p WHERE p.pseudo_player = ?1")
+    Optional<PlayerEntity> findById(String pseudo_player);
+}
