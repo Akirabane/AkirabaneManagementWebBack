309bb93362452be4408aab2bbb92195c38ddec0c
Akirabane <joshuabenon988@yahoo.fr>
2023-03-14 15:48:01 +0100

    added test and exception to main class

---
+++ src/main/java/fr/akirabane/AkirabaneManagementWebBack/Main.java
@@ -0,0 +1,20 @@
+package fr.akirabane.AkirabaneManagementWebBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@SpringBootApplication
+public class Main {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
