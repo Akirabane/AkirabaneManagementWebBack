95001a48cba835938bf6037e7ea1cf51e2239955
Akirabane <joshuabenon988@yahoo.fr>
2023-03-14 11:05:18 +0100

    Initial commit

---
+++ src/main/java/fr/akirabane/TestSpringFS/TestSpringFsApplication.java
@@ -0,0 +1,13 @@
+package fr.akirabane.TestSpringFS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringFsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringFsApplication.class, args);
+	}
+
+}
