18b523397aa05a6f3972e6797bc2e4820e04588c
Akirabane <joshuabenon988@yahoo.fr>
2023-03-14 11:06:55 +0100

    Created test files

---
+++ src/test/java/fr/akirabane/TestSpringFS/TestSpringFsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.akirabane.TestSpringFS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringFsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
